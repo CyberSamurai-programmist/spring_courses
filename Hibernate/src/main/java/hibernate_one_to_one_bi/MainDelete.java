eacc2e7b8d43687576180f6425b154583d0b6f93
samurai <mathewsloban@yandex.com>
2021-08-27 08:08:06 +0300

    Add directory hibernate_one_to_one_uni

---
+++ Hibernate/src/main/java/hibernate_one_to_one_bi/MainDelete.java
@@ -0,0 +1,34 @@
+package hibernate_one_to_one_bi;
+
+import hibernate_one_to_one_bi.entity.Detail;
+import hibernate_one_to_one_bi.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class MainDelete {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+
+        Session session = null;
+
+        try {
+            session = factory.getCurrentSession();
+
+            session.beginTransaction();
+            Detail detail = session.get(Detail.class,1);
+            detail.getEmployee().setEmpDetail(null); // при отсутсвии cascade
+
+            session.delete(detail);
+            session.getTransaction().commit();
+            System.out.println("Done!");
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
